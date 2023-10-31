e1a2d3884ac7a93ccef07efc9c8788146f139b0b
yk_dz <3070799584@qq.com>
2023-10-31 22:04:54 +0800

    First Push

--- src/main/java/cn/encmys/ykdz/forest/getoutoffmychest/listener/ChestListener.java
+++ src/main/java/cn/encmys/ykdz/forest/getoutoffmychest/listener/ChestListener.java
@@ -42,7 +42,6 @@ public class ChestListener implements Listener {
         Iterator<Entity> iterator = entities.iterator();
 
         boolean flag = false;
-
         while(iterator.hasNext()) {
             Entity entity = iterator.next();
             if(entity.getType() == EntityType.CAT && ((Sittable) entity).isSitting()) {
