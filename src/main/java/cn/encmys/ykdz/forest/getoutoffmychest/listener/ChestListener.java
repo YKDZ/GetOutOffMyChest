797a49265445cb9daba345aebc91e77e14352a6a
YKDZ <3070799584@qq.com>
2023-11-01 10:21:16 +0800

    First Push

--- src/main/java/cn/encmys/ykdz/forest/getoutoffmychest/listener/ChestListener.java
+++ src/main/java/cn/encmys/ykdz/forest/getoutoffmychest/listener/ChestListener.java
@@ -37,7 +37,7 @@ public class ChestListener implements Listener {
 
         Player p = e.getPlayer();
         Block b = e.getClickedBlock();
-        Vector vec = ((Chest) b.getBlockData()).getFacing().getOppositeFace().getDirection().normalize().multiply(speed).add(new Vector(0, speed * angle / 45, 0));
+        Vector vec = ((Chest) b.getBlockData()).getFacing().getOppositeFace().getDirection().add(new Vector(0, 1, 0)).normalize().multiply(speed);
         Collection<Entity> entities = EntityUtils.getEntitiesAboveBlock(b, 1d);
         Iterator<Entity> iterator = entities.iterator();
 
